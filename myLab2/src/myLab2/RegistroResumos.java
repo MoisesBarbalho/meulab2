e638ff389a46a4a4c900a9db17b335f2bea23586
Particular <Particular@DESKTOP-85EBQGN>
2024-12-22 22:27:18 -0300

    classes RegistroResumo e (auxiliar) Resumo

--- myLab2/src/myLab2/RegistroResumos.java
+++ myLab2/src/myLab2/RegistroResumos.java
@@ -6,10 +6,10 @@ public class RegistroResumos {
 	private  int capacidade;
 	private int contagem;
 	
-	public RegistroResumos(int capacidadeDeResumos) {
+	public RegistroResumos(Aluno aluno, int capacidadeDeResumos) {
 		this.capacidade = capacidadeDeResumos;
 		this.resumos = new LimitedHashMap<String, Resumo>(this.capacidade);
-		this.contagem = 0; 
+		this.contagem = this.resumos.size(); 
 	}
 	public void adicionaResumo(String tema, String conteudo) throws ResumoJaAdicionadoException {
 		if(this.resumos.containsKey(tema))
@@ -17,4 +17,59 @@ public class RegistroResumos {
 		Resumo novoResumo = new Resumo(tema, conteudo);
 		this.resumos.put(tema, novoResumo);
 	}
+	public void removeResumo(String tema) {
+		if(!(this.resumos.containsKey(tema)))
+			throw new NoSuchElementException("Resumo não encontrado");
+		this.resumos.remove(tema);
+	}
+	public int conta() {
+		return this.contagem;
+	}
+	public String[] pegaResumos() {
+		return (String[])this.resumos.values()
+				.stream()
+				.map(Resumo::toString).toArray();
+	}
+	public String imprimeResumos() {
+		return String.join(" | ", this.pegaResumos());
+	}
+	public boolean temResumo(String tema) {
+		return this.resumos.containsKey(tema);
+	}
+	public String[] buscaResumo(String chaveDeBusca) {
+		String chave = chaveDeBusca.toLowerCase();
+		String[] encontrados = (String[]) this.resumos.values()
+				.stream()
+				.filter(resumo -> resumo.getConteudo().toLowerCase().contains(chave))
+				.toArray();
+		return encontrados;
+	}
+	private String[] getTemas() {
+		return (String[]) this.resumos.keySet()
+				.stream().toArray();
+	}
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("---Registro de Resumos---\n")
+		.append("Capacidade: " + this.capacidade + "\n")
+		.append("Registrados:" + this.contagem + "\n")
+		.append("Temas:\n " + String.join(".\n",this.getTemas()));
+		return sb.toString(); 
+	}
+	@Override
+	public boolean equals(Object o) {
+		if(!(o instanceof RegistroResumos)) return false;
+		if(o == this) return true;
+		
+		RegistroResumos oRR = (RegistroResumos) o;
+		return (this.imprimeResumos().contentEquals(oRR.imprimeResumos()));
+	}
+	@Override
+	public int hashCode() {
+		int hash = 19;
+		hash = 31 * hash + this.resumos.hashCode();
+		return hash;
+	}
+	
 }
--- myLab2/src/myLab2/Resumo.java
+++ myLab2/src/myLab2/Resumo.java
@@ -8,4 +8,34 @@ public class Resumo {
 		this.tema = tema;
 		this.conteudo = conteudo;
 	}
+	public String getTema() {
+		String title = this.tema;
+		return title;
+	}
+	public String getConteudo() {
+		String content = this.conteudo;
+		return content;
+	}
+	public void setConteudo(String novoConteudo) {
+		this.conteudo = novoConteudo;
+	}
+	@Override
+	public String toString() {
+		String temaEConteudo = this.tema + ": " + this.conteudo;
+		return temaEConteudo;
+	}
+	@Override
+	public boolean equals(Object o) {
+		if(!(o instanceof Resumo)) return false;
+		if(o == this) return true;
+		
+		Resumo oR = (Resumo) o;
+		return (this.tema.contentEquals(oR.getTema()));
+	}
+	@Override
+	public int hashCode() {
+		int hash = 19;
+		hash = 31 * hash + this.tema.hashCode();
+		return hash;
+	}
 }
